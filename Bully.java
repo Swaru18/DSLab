9c46a1f18d0c916f8d02c6cf4eecad497d7804ff
Swarangi Aher <114857502+Swaru18@users.noreply.github.com>
2026-05-13 14:46:06 +0530

    Add files via upload

--- Bully.java
+++ Bully.java
@@ -1,125 +1,64 @@
-import java.io.InputStream; 
-import java.io.PrintStream; 
-import java.util.Scanner; 
-public class Bully  
-{ 
-static boolean[] state = new boolean[5]; 
-int coordinator; 
-public static void up(int up)//4  
-{ 
-if (state[up - 1])// 0 1 2 3 4  
-{ 
+import java.util.*;
 
-  System.out.println("process" + up + "is already up"); 
-        }  
-        else  
-        { 
-            int i; 
-            Bully.state[up - 1] = true; 
-            System.out.println("process " + up + "held election"); 
-            for (i = up; i < 5; ++i)  
-            { 
-                System.out.println("election message sent from process" + up + "to process" + (i + 1)); 
-            } 
-            for (i = up + 1; i <= 5; ++i)  
-            { 
-                if (!state[i - 1]) continue; 
-                System.out.println("alive message send from process" + i + "to process" + up); 
-                break; 
-            } 
-        } 
-    } 
- 
-    public static void down(int down) 
-    { 
-        if (!state[down - 1]) 
-        { 
-            System.out.println("process " + down + "is already dowm."); 
-        }  
-        else  
-        { 
-            Bully.state[down - 1] = false; 
-        } 
+public class Bully {
+
+    static boolean p[] = {true, true, true, true, true};
+
+    static void msg(int n) {
+
+        if (!p[n - 1]) {
+            System.out.println("Process down");
+            return;
+        }
+
+        if (p[4]) {
+            System.out.println("OK");
+            return;
+        }
+
+        System.out.println("Election started by " + n);
+
+        for (int i = n; i < 5; i++)
+            System.out.println(n + " -> " + (i + 1));
+
+        for (int i = 5; i >= 1; i--) {
+
+            if (p[i - 1]) {
+                System.out.println("Coordinator is " + i);
+                break;
+            }
+        }
     }
-      public static void mess(int mess)  
-    { 
-        if (state[mess - 1]) 
-        { 
-            if (state[4])  
-            { 
-                System.out.println("0K"); 
-            }  
-            else if (!state[4])  
-            { 
-                int i; 
-                System.out.println("process" + mess + "election"); 
-                for (i = mess; i < 5; ++i)  
-                { 
-                    System.out.println("election send from process" + mess + "to process " + (i + 1)); 
-                } 
-                for (i = 5; i >= mess; --i) 
-                   { 
-                    if (!state[i - 1]) continue; 
-                    System.out.println("Coordinator message send from process" + i + "to all"); 
-                    break; 
-                } 
-            } 
-        }  
-        else  
-        { 
-            System.out.println("Prccess" + mess + "is down"); 
-        } 
-    } 
- 
-    public static void main(String[] args)  
-    { 
-        int choice; 
-        Scanner sc = new Scanner(System.in); 
-        for (int i = 0; i < 5; ++i)  
-        { 
-            Bully.state[i] = true; 
-        } 
-        System.out.println("5 active process are:"); 
-        System.out.println("Process up  = p1 p2 p3 p4 p5"); 
-        System.out.println("Process 5 is coordinator"); 
-        do  
-        { 
-            System.out.println("........."); 
-            System.out.println("1 up a process."); 
-            System.out.println("2.down a process"); 
-            System.out.println("3 send a message"); 
-            System.out.println("4.Exit"); 
-            choice = sc.nextInt(); 
-            switch (choice)  
-            { 
-                case 1:  
-                { 
-                    System.out.println("bring proces up"); 
-                    int up = sc.nextInt(); 
-                    if (up == 5)  
-                    { 
-                        System.out.println("process 5 is co-ordinator"); 
-                        Bully.state[4] = true; 
-                        break; 
-                    } 
-                    Bully.up(up); 
-                    break; 
-                } 
-                 
-                case 2: 
-                 { 
-                    System.out.println("bring down any process."); 
-                    int down = sc.nextInt(); 
-                    Bully.down(down); 
-                    break; 
-                } 
-                case 3:  
-                { 
-                    System.out.println("which process will send message"); 
-                    int mess = sc.nextInt(); 
-                    Bully.mess(mess); 
-                } 
-            } 
-        } while (choice != 4); 
-    } 
-} 
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("\n1.Process Down\n2.Process Up\n3.Send a Message\n4.Exit");
+
+            int ch = sc.nextInt();
+
+            if (ch == 4)
+                return;
+
+            System.out.print("Enter process: ");
+            int n = sc.nextInt();
+
+            switch (ch) {
+
+                case 1:
+                    p[n - 1] = false;
+                    break;
+
+                case 2:
+                    p[n - 1] = true;
+                    break;
+
+                case 3:
+                    msg(n);
+            }
+        }
+    }
+}
